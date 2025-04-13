dc65a1f5ed0f8a9451812a9a4ca500f4891cd2c7
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-13 14:51:45 +0700

    add test for channel as parameter

--- channel_test.go
+++ channel_test.go
@@ -21,3 +21,18 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Arya Rizki Andaru"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+}
