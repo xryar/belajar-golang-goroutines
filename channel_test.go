7f0e9af05df568b875f9a418d0d7457ea75c9bef
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-13 19:08:23 +0700

    add select channel example

--- channel_test.go
+++ channel_test.go
@@ -104,3 +104,30 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("selesai")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Menerima data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Menerima data dari channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
