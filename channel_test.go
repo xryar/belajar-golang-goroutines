339a737cb47ec7a2f401b7c1a95e872029b0ecf2
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-13 16:37:27 +0700

    add range channel example

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -82,6 +83,24 @@ func TestBufferedChannel(t *testing.T) {
 
 	fmt.Println(cap(channel))
 	fmt.Println(len(channel))
+
+	fmt.Println("selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
 
 	fmt.Println("selesai")
 }
